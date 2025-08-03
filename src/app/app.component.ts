9a90dc50a669134f91afbc8dbbbd036c0dd53c5d
matiasArt <matias.arturi@gmail.com>
2025-08-03 15:03:19 -0300

    diferentes temas para diferentes componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule,MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
